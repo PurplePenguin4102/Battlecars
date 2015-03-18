a87c2ec36a3bcbd07233c522f06c6ba2fed31e82
PurplePenguin4102 <joseph.h.ray@gmail.com>
2015-03-18 15:47:09 +1000

    still tryin boss

--- Car.java
+++ Car.java
@@ -1,4 +1,46 @@
 
 public class Car {
-
+	
+	/** This class is the controller for our cars.
+	 *  Cars should be able to accelerate, slow down, change direction
+	 *  change gear, activate weapons, 
+	 */
+	private static Integer speed = 0;
+	private static Integer rpm = 0;
+	private static Integer tyreForce = 0;
+	private static Integer tyrerpm = 0;
+	private static Integer rpmMax;
+	
+	public Car() {
+	rpmMax = 6000;
+	}
+	
+	public static void accelPedal (boolean pedal){
+		
+		if (pedal && rpm < rpmMax) {
+			rpm++;
+		}
+		if (!pedal) {
+			rpm--;
+		}
+		
+	}
+	
+	private static void calcTyreForce(int rpm){
+		
+		
+	}
+	
+	public static void changeDir (int x){
+		
+	}
+	
+	public static void changeGear (int x){
+		
+	}
+	
+	public static void useWeapon (int x){
+		
+	}
+	
 }
---
+++ UseCar.java
@@ -0,0 +1,9 @@
+
+public class UseCar {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
